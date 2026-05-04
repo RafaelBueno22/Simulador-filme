6ff7e9d732a32202b5af7f56f1b9b85fc7ac6ac1
RafaelBueno22 <rafaelsenac21@gmail.com>
2026-05-04 16:20:44 -0300

    Add files via upload

--- SIMULADORFILMES-main/script.js
+++ SIMULADORFILMES-main/script.js
@@ -8,7 +8,7 @@ function calcular(){
     let seven = document.getElementById("Seven")
     let duna = document.getElementById("Duna")
     let sociedade = document.getElementById("Sociedade")
-    let interestelar = document.getElementById("Interestelar")
+    let interestelar = document.getElementById("Interstelar")
     //2 variaveis iniciais
     //total começa com 0
     let total = 0
@@ -34,7 +34,7 @@ function calcular(){
     }
     if(duna.checked){
         total = total + 40.00
-        itens = itens + "Duna <br>"
+        itens = itens + "Duna 2 <br>"
     }
     if(sociedade.checked){
         total = total + 30.00
@@ -43,7 +43,7 @@ function calcular(){
     }
     if(interestelar.checked){
         total = total + 35.00
-        itens = itens + "Interestelar<br>"
+        itens = itens + "Interstelar<br>"
     }
 
     //4. pegar div do resultado
